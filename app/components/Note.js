83723f4a998de6e30bf6c2b9166c336221729d56
Charanb06 <46635742+Charanb06@users.noreply.github.com>
2019-07-31 22:52:44 -0400

    Create Note.js

---
+++ app/components/Note.js
@@ -0,0 +1,55 @@
+import * as React from 'react';
+import { Text, View, StyleSheet, TouchableOpacity } from 'react-native';
+import { Constants } from 'expo';
+
+
+export default class Note extends React.Component {
+  render() {
+    return (
+      <View style={styles.note}>
+      
+        <Text style={styles.noteText}>{this.props.valDate}</Text>
+        <Text style={styles.noteText}>{this.props.valNote}</Text>
+
+
+        <TouchableOpacity onPress={this.props.deleteMethod} style={styles.noteDelete}>
+
+        <Text style={styles.noteDeleteText}>
+        D
+        </Text>
+
+        </TouchableOpacity>
+
+
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  note:{
+    position:"relative",
+    padding:20,
+    paddingRight:100,
+    borderBottomWidth:2,
+    borderBottomColor:"#effded",
+  },
+  noteText:{
+    paddingLeft:20,
+    borderLeftWidth:10,
+    borderLeftColor:"#E98B63",
+  },
+  noteDelete:{
+    position:"absolute",
+    justifyContent:"center",
+    alignItems:"center",
+    backgroundColor:"#9983b9",
+    padding:10,
+    bottom:10,
+    right:10,
+
+  },
+  noteDeleteText:{
+    color:"white",
+  }
+});

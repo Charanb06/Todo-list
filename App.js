9f608715c216d3f523065afde0fdc9cd24801de1
Charanb06 <46635742+Charanb06@users.noreply.github.com>
2019-07-31 22:49:02 -0400

    Create App.js

---
+++ App.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import Main from './app/components/Main';
+import Note from './app/components/Note';
+
+
+
+export default class App extends React.Component {
+       render () {
+        return (
+          <Main/>
+        );
+       }
+}

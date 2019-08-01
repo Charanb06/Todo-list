a6096792b0f71342145b620733e3fe50df1dd61f
Charanb06 <46635742+Charanb06@users.noreply.github.com>
2019-07-31 22:49:58 -0400

    Add files via upload

--- App.js
+++ App.js
@@ -1,13 +1,12 @@
-import React, {Component} from 'react';
+import React from 'react';
 import Main from './app/components/Main';
-import Note from './app/components/Note';
-
-
 
 export default class App extends React.Component {
-       render () {
-        return (
-          <Main/>
-        );
-       }
+  render() {
+    return (
+      <Main />
+    );
+  }
 }
+
+
